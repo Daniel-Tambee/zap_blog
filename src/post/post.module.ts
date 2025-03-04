2cd7d79307e5574eb995367ac60fa8c4132f45ab
Daniel-Tambee <danieltambee@gmail.com>
2025-03-04 13:22:32 +0100

    fix/dependency things

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { DbService } from 'src/db/db.service';
 
 @Module({
   controllers: [PostController],
-  providers: [PostService]
+  providers: [PostService, DbService],
 })
 export class PostModule {}
--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TagsService } from './tags.service';
 import { TagsController } from './tags.controller';
+import { DbService } from 'src/db/db.service';
 
 @Module({
   controllers: [TagsController],
-  providers: [TagsService]
+  providers: [TagsService, DbService],
 })
 export class TagsModule {}
