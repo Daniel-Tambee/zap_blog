ee0d81afc00ab4d305aa96c7d309457220425502
Daniel-Tambee <danieltambee@gmail.com>
2025-03-06 14:56:15 +0100

    fix/core fix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Allows all origins (change to specific domains if needed)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
